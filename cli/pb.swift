a52d47b638c27fccdf9ce134a99a508f381d31ce
Artem Chistyakov <chistyakov.artem@gmail.com>
2021-04-21 00:36:11 -0400

    Add Markdown table support to pb.

--- cli/pb.swift
+++ cli/pb.swift
@@ -8,6 +8,12 @@ func toJSON(dict: NSDictionary) -> String {
     return ""
 }
 
+extension String {
+    var markdownTableSafe: String {
+        self.replacingOccurrences(of: "|", with: "&#124;")
+    }
+}
+
 let pasteboard = NSPasteboard.general
 
 let json: NSDictionary = [
@@ -28,6 +34,32 @@ let json: NSDictionary = [
     }() as String? as Any,
     "text": pasteboard.string(forType: NSPasteboard.PasteboardType.string) as Any,
     "url": pasteboard.string(forType: NSPasteboard.PasteboardType.URL) as Any,
+    "asMarkdownTable": {
+        if let lines = pasteboard.string(forType: NSPasteboard.PasteboardType.string)?.components(separatedBy: "\n"),
+           lines.count > 0 {
+            let inputTable = lines.map { $0.components(separatedBy: "\t") }
+            let headerColumns = inputTable.first!
+            let maxWidths = headerColumns.enumerated().map { (index, _) in
+                inputTable.map { index < $0.count ? $0[index].unicodeScalars.count : 0 }.max()
+            }
+
+            let renderedRows = inputTable.map { row in
+                row
+                    .prefix(headerColumns.count)
+                    .enumerated()
+                    .map { (index, col) in col.markdownTableSafe.padding(toLength: maxWidths[index]!, withPad: " ", startingAt: 0) }
+                    .joined(separator: " | ")
+            }
+
+            let divider = headerColumns
+                .enumerated()
+                .map { (index, _) in String(repeating: "-", count: maxWidths[index]!) }
+                .joined(separator: " | ")
+
+            return "| \(renderedRows.first!) |\n| \(divider) |\n| \(renderedRows.dropFirst(1).joined(separator: " |\n| ")) |"
+        }
+        return nil
+    }() as String? as Any
 ]
 
 print(toJSON(dict: json))
